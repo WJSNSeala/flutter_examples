6f8a535a93288a389379e20ce385f89ba696ee71
WJSNSeala <harhyom9189@gmail.com>
2024-12-26 16:49:13 +0900

    recipe app complete

--- recipe_sample/lib/main.dart
+++ recipe_sample/lib/main.dart
@@ -21,7 +21,7 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Padding titleSection = Padding(
+    Widget titleSection = Padding(
       padding: const EdgeInsets.all(24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -33,7 +33,7 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
     );
-    Row iconSection = Row(
+    Widget iconSection = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         buildIconRow(
@@ -53,16 +53,52 @@ class MyHomePage extends StatelessWidget {
         ),
       ],
     );
+    Widget stepSection = Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '조리순서',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.only(right: 8), child: Text("1", style: Theme.of(context).textTheme.titleLarge,),),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  "[양파브로콜리 볶음] 양파와 브로콜리를 잘게 잘라준다.",
+                  style: TextStyle(
+                    fontSize: 16
+                  )
+                )
+              ),
+              Image.asset('assets/step01.png', width: MediaQuery.of(context).size.width * 0.3, fit: BoxFit.contain),
+
+            ],
+          ),
+          buildIconRow(Icons.lightbulb, '크기가 작은 브로콜리를 사용해도 좋아요', Colors.teal),
+          buildIconRow(Icons.shopping_cart, '라오메뜨 천연세라믹 양면 도마', Colors.grey)
+        ],
+      )
+    );
+
 
     return Scaffold(
       appBar: AppBar(
         title: Text('recipe example'),
       ),
-      body: Column(
+      body: ListView(
         children: [
           Image.asset('assets/image01.png'),
           titleSection,
           iconSection,
+          stepSection,
         ],
       ),
     );
